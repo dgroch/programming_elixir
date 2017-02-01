fcd472060908027451465249afdc1ed5e62a8435
Dan <dan@heroboyfriend.com>
2017-02-01 21:26:12 +0000

    FizzBuzz solution with no conditional logic

--- lib/5_anonymous_functons.exs
+++ lib/5_anonymous_functons.exs
@@ -3,16 +3,9 @@
    {_,    error}  -> "Error: #{:file.format_error(error)}"
  end
 
- IO.puts handle_open.(File.open("not here"))
- 
-@doc """
-  Exercise : Functions-2
-  Write a function that takes three arguments.
-  If the first two are zero, return "FizzBuzz".
-  If there first is zero, return "Fizz".
-  If the second is Zero, return "Buzz".
-  Otherwise return the third argument.
-"""
+IO.puts handle_open.(File.open("not here"))
+
+# Exercise: Functions-2
 fizz_buzz = fn
    (0, 0, _)  -> "FizzBuzz"
    (0, _, _)  -> "Fizz"
@@ -24,3 +17,16 @@ IO.puts fizz_buzz.(0, 0, "str")
 IO.puts fizz_buzz.(0, "str", "str")
 IO.puts fizz_buzz.("str", 0, "str")
 IO.puts fizz_buzz.("str", "str", "str")
+
+# Exercise: Functions-3
+invoke_fizz_buzz = fn n -> fizz_buzz.(rem(n, 3), rem(n, 5), n) end
+
+IO.puts invoke_fizz_buzz.(10)
+IO.puts invoke_fizz_buzz.(11)
+IO.puts invoke_fizz_buzz.(12)
+IO.puts invoke_fizz_buzz.(13)
+IO.puts invoke_fizz_buzz.(14)
+IO.puts invoke_fizz_buzz.(15)
+IO.puts invoke_fizz_buzz.(16)
+IO.puts invoke_fizz_buzz.(17)
+
